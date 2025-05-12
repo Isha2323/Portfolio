2b7ccf827d38f2a20a4ac9dca979fa30b0226ce8
Isha2323 <ishasitap112@gmail.com>
2025-05-12 18:05:01 +0530

    Final Changes

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
-  images: {
-    unoptimized: true, // Required because image optimization needs a server
-  },
-  basePath: "/Portfolio.git",
 };
 
 export default nextConfig;
